b6ca4dd2095be6b389a8265c82429bc6cc92d089
tomji638 <125173523+tomji638@users.noreply.github.com>
2025-06-05 01:38:30 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  root: '.',
   build: {
-    outDir: '../dist'
+    outDir: 'dist'  // ✅ 输出到当前项目下的 dist 目录
   }
 });
+
